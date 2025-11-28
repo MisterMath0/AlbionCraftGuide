8f2ff2aca73c85a62f74d796a9b2a7192987d14c
MisterMath0 <guymathieuf@gmail.com>
2025-11-28 21:18:37 +0100

    chore: package updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
